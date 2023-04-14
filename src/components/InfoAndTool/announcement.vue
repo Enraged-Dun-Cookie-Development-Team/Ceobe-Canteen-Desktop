66239be07ec961392d3e2acc69b1cfad96aad6f8
kazeliu <kaze.liu@qq.com>
2023-04-14 17:49:37 +0800

    添加公告

--- src/components/InfoAndTool/announcement.vue
+++ src/components/InfoAndTool/announcement.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="announcement">
-
+    <v-carousel :show-arrows="false">
+      <v-carousel-item
+          v-for="(item,i) in announcementInfo.data"
+          :key="i"
+      >
+        <div v-html="item.html"></div>
+      </v-carousel-item>
+    </v-carousel>
   </div>
 </template>
 
 <script setup name="announcement">
 
+import {onMounted, reactive} from "vue";
+import {changeToCCT} from "@/utils/timeUtil";
+
+const announcementInfo = reactive({
+  data: [],
+  getData() {
+    window.ceobeRequest.getAnnouncementInfo().then(res => {
+      announcementInfo.data = res.data.filter(
+          (x) =>
+              new Date(x.start_time) <= changeToCCT(new Date()) &&
+              new Date(x.over_time) >= changeToCCT(new Date())
+      );
+    })
+  }
+})
+
+onMounted(() => {
+  announcementInfo.getData();
+})
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
--- src/view/home/infoAndTool.vue
+++ src/view/home/infoAndTool.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="info-and-tool">
     <!--常用信息-->
-    <weekly-quest></weekly-quest>
+<!--    <weekly-quest></weekly-quest>-->
     <!--公告-->
-    <announcement v-model="announcementInfo.data"></announcement>
+    <announcement ></announcement>
     <!--广告-->
     <!--发源地-->
     <!--快捷工具-->
@@ -18,23 +18,12 @@ import {changeToCCT} from "@/utils/timeUtil";
 import Announcement from "@/components/InfoAndTool/announcement";
 import WeeklyQuest from "@/components/InfoAndTool/weeklyQuest";
 
-const announcementInfo = reactive({
-  data: [],
-  getData() {
-    window.ceobeRequest.getAnnouncementInfo().then(res => {
-      announcementInfo.data = res.data.filter(
-          (x) =>
-              new Date(x.start_time) <= changeToCCT(new Date()) &&
-              new Date(x.over_time) >= changeToCCT(new Date())
-      );
-    })
-  }
-})
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
 .info-and-tool {
   width: 500px;
   max-width: 500px;
+  user-select: none;
 }
 </style>
