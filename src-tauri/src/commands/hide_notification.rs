3091aea0f7aef67b650af5689a81cc757bfb0d5f
ForzenString <964413011@qq.com>
2023-09-07 23:38:55 +0800

    :sparkles: 将平台相关导入限制

--- src-tauri/src/commands/hide_notification.rs
+++ src-tauri/src/commands/hide_notification.rs
@@ -1,13 +1,14 @@
 use tauri::{command, Window};
-use windows::Win32::Graphics::Gdi::UpdateWindow;
-use windows::Win32::UI::WindowsAndMessaging::{
-    GetWindowLongPtrW, SetWindowLongPtrW, GWL_EXSTYLE, WS_EX_APPWINDOW, WS_EX_TOOLWINDOW,
-};
+
 
 #[command]
 pub fn hide_notification(window: Window) -> tauri::Result<()> {
     #[cfg(windows)]
     {
+        use windows::Win32::Graphics::Gdi::UpdateWindow;
+        use windows::Win32::UI::WindowsAndMessaging::{
+            GetWindowLongPtrW, SetWindowLongPtrW, GWL_EXSTYLE, WS_EX_APPWINDOW, WS_EX_TOOLWINDOW,
+        };
         let hwnd = window.hwnd()?;
         let mut style = unsafe { GetWindowLongPtrW(hwnd, GWL_EXSTYLE) };
         style |= !WS_EX_APPWINDOW.0 as isize;
