f2669c094f00dd66a05c3c501c681dc5c949732d
洛梧藤 <2957035701@qq.com>
2025-01-05 00:51:56 +0800

    👔 转运营接口到cdn，更改工具接口

--- src/api/resourceFetcher/toolkits.ts
+++ src/api/resourceFetcher/toolkits.ts
@@ -1,24 +1,73 @@
 import requestClient, {Payload, Response} from "@/utils/requestUtil.js";
 
-
 export interface Datum {
     /**
-     * 头像
+     * logo
      */
-    avatar: string;
+    icon_url: string;
     /**
-     * 跳转链接
+     * id
      */
-    jump_url: string;
+    id: string;
+    links: Link[];
+    /**
+     * 描述
+     */
+    localized_description: LocalizedLanguage;
     /**
      * 名字
      */
-    nickname: string;
+    localized_name: LocalizedLanguage;
+    /**
+     * slogen
+     */
+    localized_slogan: LocalizedLanguage;
+    /**
+     * tags
+     */
+    localized_tags: LocalizedTags;
 }
+
+export interface Link {
+    /**
+     * 链接名字
+     */
+    localized_name: LocalizedLanguage;
+    /**
+     * 是否为主要链接
+     */
+    primary?: boolean;
+    /**
+     * 区域，枚举(CHINA_MAINLAND, EXCEPT_CHINA_MAINLAND)
+     */
+    regionality: string;
+    /**
+     * 链接
+     */
+    url: string;
+}
+
+/**
+ * 多语言
+ */
+export interface LocalizedLanguage  {
+    en_US: string;
+    zh_CN: string;
+}
+
+/**
+ * tags
+ */
+export interface LocalizedTags {
+    en_US: string[];
+    zh_CN: string[];
+}
+
 
 export function getToolsLinks(): Promise<Response<Payload<Datum[]>>> {
     return requestClient.requestPayload({
-        url: "/canteen/operate/toolLink/list", method: "GET"
-
+        url: "/cdn/operate/toolLink/list", 
+        method: "GET",
+        requestTarget: 'ServeCDN',
     });
 }
