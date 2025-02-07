1e15a166530ef3812662d97d8d2725b6b95c330f
FrozenString <964413011@qq.com>
2025-02-07 11:03:08 +0800

    :feat:调整更新界面布局

--- src/components/DownloadSource.vue
+++ src/components/DownloadSource.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { computed, ref } from "vue";
+
 import operate from "@/api/operations/operate";
 import {
   DownloadSource,
@@ -6,14 +8,10 @@ import {
   PrimaryUrl,
   SpareUrl,
 } from "@/api/resourceFetcher/version";
-import { log } from "console";
-import { computed, ref } from "vue";
 
 const props = defineProps<{
   source: DownloadSource;
 }>();
-
-const initialSelect = ref(1);
 
 const downloadSourceCombine = (primary: PrimaryUrl, spares?: SpareUrl[]) => {
   const urls: SpareUrl[] = [];
@@ -21,39 +19,48 @@ const downloadSourceCombine = (primary: PrimaryUrl, spares?: SpareUrl[]) => {
   if (spares) urls.push(...spares);
   return urls;
 };
-const urls = computed(() =>
-  downloadSourceCombine(props.source.primary_url, props.source.spare_urls).map(
-    (url) => {
-      return url.url;
-    },
-  ),
-);
+const urls = downloadSourceCombine(
+  props.source.primary_url,
+  props.source.spare_urls,
+).map((url) => {
+  return url.url;
+});
 const sourceItem = computed(() =>
-  downloadSourceCombine(props.source.primary_url, props.source.spare_urls).map(
-    (url) => {
-      console.log(url);
-
-      return {
-        title: url.name === "Primary" ? "主链接" : url.name,
-        subtitle: `手动下载：${url.manual} | 支持平台：${url.support_platforms ?? [].join(", ")}`,
-      };
-    },
-  ),
+  downloadSourceCombine(props.source.primary_url, props.source.spare_urls),
 );
+
+const itemProp = (item: SpareUrl) => {
+  return {
+    title: item.name === "Primary" ? "主链接" : item.name,
+    subtitle: `手动下载：${item.manual} | 支持平台：${item.support_platforms ?? [].join(", ")}`,
+  };
+};
+
+const select = ref(sourceItem.value[0]);
 </script>
 
 <template>
   <v-col>
+    <p>{{ source.name }}</p>
     <p v-if="source.description">{{ source.description }}</p>
-    <VSelect>
-      density="compact" lable="下载源" v-model="initialSelect"
-      :item="sourceItem"
-    </VSelect>
-    <v-btn
-      color="#e6a23c"
-      icon="mdi mdi-check"
-      @click="operate.openUrlInBrowser(urls[initialSelect - 1])"
-    />
+    <v-row class="pa-4">
+      <VSelect
+        v-model="select"
+        density="compact"
+        label="下载源"
+        :items="sourceItem"
+        :item-props="itemProp"
+        :hint="`手动下载：${select.manual} | 支持平台：${select.support_platforms ?? [].join(', ')}`"
+        single-line
+      >
+      </VSelect>
+      <v-btn
+        class="ml-4"
+        color="#e6a23c"
+        icon="mdi mdi-arrow-right"
+        @click="operate.openUrlInBrowser(select.url)"
+      />
+    </v-row>
   </v-col>
 </template>
 
